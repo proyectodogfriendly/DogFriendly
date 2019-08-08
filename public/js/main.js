99168d6bbdd0ffe0fdf31eac588f2dd4b8a7895a
RebecaLS <rebeca.lopez.sae@gmail.com>
2019-08-08 12:35:17 +0200

    cambios scroll

--- public/js/main.js
+++ public/js/main.js
@@ -15,14 +15,17 @@
         - Configurar VivusSVG para que trace las líneas, y asociar un callback que muestre la imagen previamente ocultada
 */
 
-(function() {
+
+(function () {
+
   let winScroll;
-  const mobiles = document.querySelectorAll(".mobile-inertia"),
-    messages = document.querySelectorAll(".message-inertia"),
-    mobilescreens = document.querySelectorAll(".mouse-inertia"),
-    navigation = document.querySelector(".navigation"),
-    bgDark = document.querySelector(".bg-dark"),
-    sliderTrack = document.querySelector(".slider-track");
+  const mobiles = document.querySelectorAll('.mobile-inertia'),
+      messages = document.querySelectorAll('.message-inertia'),
+      mobilescreens = document.querySelectorAll('.mouse-inertia'),
+      navigation = document.querySelector('.navigation'),
+      bgDark = document.querySelector('.bg-dark'),
+      sliderTrack = document.querySelector('.slider-track')
+
 
   // SVG watch scene
   // new Vivus('relojSVG', { duration: 120, type: 'oneByOne', animTimingFunction: Vivus.EASE }, () => relojSVG.classList.add('done'))
@@ -39,79 +42,63 @@
   //     }
   // }
 
+
   // Scroll effects
   window.onscroll = () => {
-    winScroll = window.scrollY;
 
-    // Reveal effects
-    document.querySelectorAll(".reveal").forEach(el => {
-      isInViewport(el)
-        ? el.classList.add("visible")
-        : el.classList.remove("visible");
-    });
+      winScroll = window.scrollY
 
-    // Navbar morph
-    winScroll > 100
-      ? navigation.classList.add("sticky")
-      : navigation.classList.remove("sticky");
+      // Reveal effects
+      document.querySelectorAll('.reveal').forEach(el => {
+          isInViewport(el) ? el.classList.add('visible') : el.classList.remove('visible')
+      })
 
-    // Hero title parallax
-    isInViewport(document.querySelector(".hero-inertia"))
-      ? (document.querySelector(
-          ".hero-inertia"
-        ).style.transform = `translateY(${winScroll * 0.3}px)`)
-      : null;
+      // Navbar morph
+      winScroll > 100 ?
+          navigation.classList.add('sticky') :
+          navigation.classList.remove('sticky')
 
-    // Movile devices horizontal transitions
-    if (isInViewport(document.querySelector(".features"))) {
-      mobiles[0].style.transform = `translateX(${mobiles[0].getBoundingClientRect()
-        .top * 0.3}px)`;
-      mobiles[1].style.transform = `translateX(${mobiles[1].getBoundingClientRect()
-        .top * 0.2}px)`;
-    }
+      // Hero title parallax
+      isInViewport(document.querySelector('.hero-inertia')) ?
+          document.querySelector('.hero-inertia').style.transform = `translateY(${winScroll * .3}px)` :
+          null
 
-    // Message box transitions
-    // if (isInViewport(document.querySelector(".facts"))) {
-    //   messages[0].style.transform = `translate(${-messages[0].getBoundingClientRect()
-    //     .top * 0.2}px, ${messages[0].getBoundingClientRect().top * 0.1}px)`;
-    //   messages[1].style.transform = `translate(${messages[1].getBoundingClientRect()
-    //     .top * 0.1}px, ${messages[1].getBoundingClientRect().top * 0.05}px)`;
-    //   messages[2].style.transform = `translate(${messages[2].getBoundingClientRect()
-    //     .top * 0.3}px, ${-messages[2].getBoundingClientRect().top * 0.05}px)`;
-    // }
+      // Movile devices horizontal transitions
+      if (isInViewport(document.querySelector('.features'))) {
+          mobiles[0].style.transform = `translateX(${mobiles[0].getBoundingClientRect().top * .3}px)`
+          mobiles[1].style.transform = `translateX(${mobiles[1].getBoundingClientRect().top * .2}px)`
+      }
 
-    // Slider transition
-    // if (isInViewport(document.querySelector(".join"))) {
-    //   sliderTrack.style.transform = `translateX(${-document
-    //     .querySelector(".join")
-    //     .getBoundingClientRect().top * 0.6}px)`;
-    // }
+      // Message box transitions
+      if (isInViewport(document.querySelector('.facts'))) {
+          messages[0].style.transform = `translate(${-messages[0].getBoundingClientRect().top * .2}px, ${messages[0].getBoundingClientRect().top * .1}px)`
+          messages[1].style.transform = `translate(${messages[1].getBoundingClientRect().top * .1}px, ${messages[1].getBoundingClientRect().top * .05}px)`
+          messages[2].style.transform = `translate(${messages[2].getBoundingClientRect().top * .3}px, ${-messages[2].getBoundingClientRect().top * .05}px)`
+      }
 
-    // Isometric slider transition
-//     isInViewport(document.querySelector(".dark-background"))
-//       ? bgDark.classList.add("on")
-//       : bgDark.classList.remove("on");
-//   };
+      // Slider transition
+      if (isInViewport(document.querySelector('.join'))) {
+          sliderTrack.style.transform = `translateX(${-document.querySelector('.join').getBoundingClientRect().top * .6}px)`
+      }
+
+      // Isometric slider transition
+      isInViewport(document.querySelector('.dark-background')) ? bgDark.classList.add('on') : bgDark.classList.remove('on')
+  }
 
   // Isometric mobile layers mouse inertia
-//   document.querySelector(".contact").onmousemove = e => {
-//     mobilescreens[0].style.transform = `translate(${e.x * 0.03}px, ${-e.y *
-//       0.03}px)`;
-//     mobilescreens[1].style.transform = `translate(${e.x * 0.06}px, ${-e.y *
-//       0.06}px)`;
-//     mobilescreens[2].style.transform = `translate(${e.x * 0.09}px, ${-e.y *
-//       0.09}px)`;
-//     mobilescreens[3].style.transform = `translate(${e.x * 0.12}px, ${-e.y *
-//       0.12}px)`;
-//   };
+  document.querySelector('.contact').onmousemove = e => {
+      mobilescreens[0].style.transform = `translate(${e.x * .03}px, ${-e.y * .03}px)`
+      mobilescreens[1].style.transform = `translate(${e.x * .06}px, ${-e.y * .06}px)`
+      mobilescreens[2].style.transform = `translate(${e.x * .09}px, ${-e.y * .09}px)`
+      mobilescreens[3].style.transform = `translate(${e.x * .12}px, ${-e.y * .12}px)`
+  }
 
   // Object viewport detection
   const isInViewport = el => {
-    const rect = el.getBoundingClientRect();
-    const vertInView =
-      rect.top <= window.innerHeight && rect.top + rect.height >= 0;
-    const horInView =
-      rect.left <= window.innerWidth && rect.left + rect.width >= 0;
-    return vertInView && horInView;
-  };
+      const rect = el.getBoundingClientRect()
+      const vertInView = (rect.top <= window.innerHeight) && ((rect.top + rect.height) >= 0)
+      const horInView = (rect.left <= window.innerWidth) && ((rect.left + rect.width) >= 0)
+      return (vertInView && horInView)
+  }
+
 })();
